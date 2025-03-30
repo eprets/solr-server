3592c794d8520dc009205854e8d9c15735335360
eprets <katya.prets@gmail.com>
2025-03-30 21:40:55 +0700

    fixes

--- CLI_CSV/src/main/java/com/example/cli_csv/service/CsvProcessor.java
+++ CLI_CSV/src/main/java/com/example/cli_csv/service/CsvProcessor.java
@@ -1,41 +1,89 @@
 package com.example.cli_csv.service;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import com.example.cli_csv.model.Book;
 import com.example.cli_csv.solr.SolrUpload;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.List;
 
 public class CsvProcessor {
     private final SolrUpload solrUploader;
+    private final String mappingPath;
 
     public CsvProcessor(String solrUrl, String mappingPath) {
         this.solrUploader = new SolrUpload(solrUrl, mappingPath);
+        this.mappingPath = mappingPath;
     }
 
-    public void processCsv(String csvPath) {
+    public void processCsv(String jsonPath) {
         try {
-            System.out.println("Start CSV...");
+            System.out.println("Start JSON...");
 
-            File csvFile = new File(csvPath);
-            CsvMapper csvMapper = new CsvMapper();
-            CsvSchema schema = CsvSchema.emptySchema().withHeader();
-
-            // Используем try-with-resources
-            try (MappingIterator<Book> iterator = csvMapper.readerFor(Book.class).with(schema).readValues(csvFile)) {
-                List<Book> books = iterator.readAll();
-                solrUploader.uploadToSolr(books);
+            // Проверка доступности Solr и ядра
+            if (!solrUploader.checkSolrAvailability()) {
+                System.out.println("Solr is not available. Exiting.");
+                return;
             }
 
-            System.out.println("The end good!");
+            if (solrUploader.checkCoreAvailability()) {
+                System.out.println("Core " + solrUploader.getCollection() + " not found. Creating core...");
+                solrUploader.createCore();
+            }
+
+            File jsonFile = new File(jsonPath);
+            if (!jsonFile.exists() || !jsonFile.isFile()) {
+                System.out.println("Invalid JSON file path: " + jsonPath);
+                return;
+            }
+
+            File mappingFile = new File(mappingPath);
+            if (!mappingFile.exists() || !mappingFile.isFile()) {
+                System.out.println("Invalid mapping file path: " + mappingPath);
+                return;
+            }
+
+            JsonFactory jsonFactory = new JsonFactory();
+            try (JsonParser jsonParser = jsonFactory.createParser(new FileInputStream(jsonFile))) {
+                while (jsonParser.nextToken() != JsonToken.END_OBJECT) {
+                    if (jsonParser.getCurrentToken() == JsonToken.START_OBJECT) {
+                        try {
+                            JsonNode bookJsonNode = parseJsonToNode(jsonParser);
+                            solrUploader.uploadToSolr(List.of(bookJsonNode)); // Отправка данных в Solr
+                        } catch (Exception e) {
+                            System.out.println("Error uploading book to Solr: " + e.getMessage());
+                        }
+                    }
+                }
+            }catch (JsonParseException e) {
+                System.out.println("Invalid JSON format: " + e.getMessage());
+            }catch (IOException e) {
+                System.out.println("Error reading JSON: " + e.getMessage());
+            }catch (Exception e) {
+                System.out.println("Unexpected error while processing JSON: " + e.getMessage());
+            }
+
+            System.out.println("JSON processing completed!");
         } catch (Exception e) {
-            System.out.println("Error CSV: " + e.getMessage());
+            System.out.println("Unexpected error: " + e.getMessage());
+        } finally {
+            System.out.println("Program finished.");
         }
     }
 
-
-
+    private JsonNode parseJsonToNode(JsonParser jsonParser) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        jsonParser.setCodec(objectMapper);
+        return objectMapper.readTree(jsonParser);
+    }
 }
--- CLI_CSV/src/main/java/com/example/cli_csv/solr/SolrUpload.java
+++ CLI_CSV/src/main/java/com/example/cli_csv/solr/SolrUpload.java
@@ -5,9 +5,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.apache.solr.common.SolrInputDocument;
-import com.example.cli_csv.model.Book;
 import com.example.cli_csv.service.MapperService;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
@@ -16,33 +21,108 @@ public class SolrUpload {
     private final MapperService mapperService;
     private final ObjectMapper objectMapper = new ObjectMapper();
     private final String collection;
+    private static final int BATCH_SIZE = 100;
+    private final String solrUrl;
+
 
     public SolrUpload(String solrUrl, String mappingPath) {
         this.solrClient = new HttpSolrClient.Builder(solrUrl).build();
         this.mapperService = new MapperService(mappingPath);
         this.collection = "books";
+        this.solrUrl = solrUrl;
     }
 
-    public void uploadToSolr(List<Book> books) throws Exception {
-        for (Book book : books) {
-            JsonNode bookJsonNode = objectMapper.valueToTree(book);
-            SolrInputDocument doc = new SolrInputDocument();
+    public boolean checkSolrAvailability() {
+        try {
+            URL url = new URL(solrUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            int responseCode = connection.getResponseCode();
+            return responseCode == HttpURLConnection.HTTP_OK;
+        } catch (IOException e) {
+            System.out.println("Solr server is not available: " + e.getMessage());
+            return false;
+        }
+    }
 
-            Iterator<String> fieldNames = bookJsonNode.fieldNames();
-            while (fieldNames.hasNext()) {
-                String jsonFieldName = fieldNames.next();
-                String solrFieldName = mapperService.getSolrFieldName(jsonFieldName);
-                JsonNode fieldValue = bookJsonNode.get(jsonFieldName);
+    public boolean checkCoreAvailability() {
+        try {
+            URL url = new URL(solrUrl + "/admin/cores?action=STATUS&core=" + collection);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            int responseCode = connection.getResponseCode();
 
-                if (fieldValue.isArray()) {
-                    fieldValue.forEach(value -> doc.addField(solrFieldName, value.asText()));
-                } else {
-                    doc.addField(solrFieldName, fieldValue.asText());
+            return responseCode == HttpURLConnection.HTTP_OK;
+        } catch (IOException e) {
+            System.out.println("Error checking core availability: " + e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean createCore() {
+        try {
+            URL url = new URL(solrUrl + "/admin/cores?action=CREATE&name=" + collection + "&configSet=sample_techproducts_configs");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            int responseCode = connection.getResponseCode();
+
+            // Проверяем, что коллекция была успешно создана
+            return responseCode == HttpURLConnection.HTTP_OK;
+        } catch (IOException e) {
+            System.out.println("Error creating core: " + e.getMessage());
+            return false;
+        }
+    }
+
+    public void uploadToSolr(List<JsonNode> booksJsonNodes) throws Exception {
+        int totalBooks = booksJsonNodes.size();
+
+        // Проверка наличия коллекции перед загрузкой
+        if (!checkCoreAvailability()) {
+            System.out.println("Core not found. Creating core...");
+            if (!createCore()) {
+                System.out.println("Failed to create core. Exiting.");
+                return;
+            } else {
+                System.out.println("Core created successfully.");
+            }
+        }
+
+        for (int i = 0; i < totalBooks; i += BATCH_SIZE) {
+            int end = Math.min(i + BATCH_SIZE, totalBooks);
+            List<JsonNode> batch = booksJsonNodes.subList(i, end);
+
+            for (JsonNode bookJsonNode : batch) {
+                SolrInputDocument doc = new SolrInputDocument();
+                Iterator<String> fieldNames = bookJsonNode.fieldNames();
+                while (fieldNames.hasNext()) {
+                    String jsonFieldName = fieldNames.next();
+                    String solrFieldName = mapperService.getSolrFieldName(jsonFieldName);
+                    JsonNode fieldValue = bookJsonNode.get(jsonFieldName);
+
+                    if ("publication_date".equals(jsonFieldName)) {
+                        try {
+                            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                            Date date = dateFormat.parse(fieldValue.asText());
+                            String formattedDate = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").format(date);
+                            doc.addField(solrFieldName, formattedDate);
+                        } catch (ParseException e) {
+                            System.out.println("Error parsing date: " + fieldValue.asText());
+                        }
+                    } else if (fieldValue.isArray()) {
+                        fieldValue.forEach(value -> doc.addField(solrFieldName, value.asText()));
+                    } else {
+                        doc.addField(solrFieldName, fieldValue.asText());
+                    }
                 }
+                solrClient.add(collection, doc);
             }
 
-            solrClient.add(collection, doc);
+            solrClient.commit(collection);
         }
-        solrClient.commit(collection);
+    }
+
+    public String getCollection() {
+        return collection;
     }
 }
